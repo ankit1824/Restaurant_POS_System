fc66be9dd6a0ebc4c44f1f1d2e9f623095b40ab2
ankit1824 <161712067+ankit1824@users.noreply.github.com>
2025-12-27 02:01:53 +0530

    Update database.js

--- Restaurant_POS_System/pos-backend/config/database.js
+++ Restaurant_POS_System/pos-backend/config/database.js
@@ -2,18 +2,13 @@ const mongoose = require("mongoose");
 const config = require("./config");
 
 const connectDB = async () => {
-  try {
-    if (!config.databaseURI) {
-      throw new Error("MONGO_URI is not defined");
+    try {
+        const conn = await mongoose.connect(config.databaseURI);
+        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`❌ Database connection failed: ${error.message}`);
+        process.exit();
     }
-
-    const conn = await mongoose.connect(config.databaseURI);
-
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error("❌ Database connection failed:", error.message);
-    process.exit(1);
-  }
-};
+}
 
 module.exports = connectDB;
