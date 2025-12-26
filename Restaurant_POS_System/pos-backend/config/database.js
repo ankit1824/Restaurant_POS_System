dbd20a2559cca9b92f16066ae9e6ea09b9f8fff2
ankit1824 <161712067+ankit1824@users.noreply.github.com>
2025-12-27 01:51:11 +0530

    Update database.js

--- Restaurant_POS_System/pos-backend/config/database.js
+++ Restaurant_POS_System/pos-backend/config/database.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
-const config = require("./config");
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(config.databaseURI);
-        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(`❌ Database connection failed: ${error.message}`);
-        process.exit();
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("❌ Database connection failed:", error.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
